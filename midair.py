a9eec3e6f529dd8d2da89ebbd873bb53b449e458
x0rnn <c0rnn@live.com>
2016-01-14 15:50:26 +0100

    Update midair.py

--- midair.py
+++ midair.py
@@ -2,7 +2,9 @@
 # On evey midair kill that counts (minheight and mindistance variables), a text message will inform everyone
 # about the distance involved, and who killed who, together with a "Holy shit" sound announcement.
 # If the map record has been broken, then the "New high score" announcement will be used instead.
-# !top will list top X map records, !mytop will list your top X map records
+# Also shows a kill counter of Killer:Victim (taken from pummel.py by mattiZed).
+# !top will list top X map records, !mytop will list your top X map records.
+# !cleartopshots (admins only) will clear all map topshots, !clearmytopshots will clear your topshots.
 # by x0rnn, mattiZed, kanzo, iou; thanks guys :)
 
 import minqlx
@@ -20,6 +22,7 @@ class midair(minqlx.Plugin):
         self.add_command(("topshots", "top"), self.cmd_topshots)
         self.add_command(("mytopshots", "mytop"), self.cmd_mytopshots)
         self.add_command("cleartopshots", self.cmd_cleartopshots, 5)
+        self.add_command("clearmytopshots", self.cmd_clearmytopshots)
 
     def handle_death(self, victim, killer, data):
         if data['KILLER'] is not None:
@@ -42,12 +45,18 @@ class midair(minqlx.Plugin):
                 if height > minheight and distance > mindistance:
                     self.db.zadd(MIDAIR_KEY.format(map_name), distance, "{},{},{}".format(k_id, v_id, int(time.time())))
                     self.db.zadd(PLAYER_KEY.format(k_id) + ":midair:" + str(map_name), distance, "{},{}".format(v_id, int(time.time())))
+                    self.db.sadd(PLAYER_KEY.format(k_id) + ":midair", v_id)
+                    self.db.incr(PLAYER_KEY.format(k_id) + ":midair:" + v_id)
+                    killer_score = self.db[PLAYER_KEY.format(k_id) + ":midair:" + v_id]
+                    victim_score = 0
+                    if PLAYER_KEY.format(v_id) + ":midair:" + k_id in self.db:
+                        victim_score = self.db[PLAYER_KEY.format(v_id) + ":midair:" + k_id]
                     if distance <= record[0][1]:
-                        msg = "{} killed {} from a distance of: ^1{} ^7units.".format(killer_name, victim_name, round(distance))
+                        msg = "{} killed {} from a distance of: ^1{} ^7units. Score: ^2{}^7:^2{}".format(killer_name, victim_name, round(distance), killer_score, victim_score)
                         self.play_sound("sound/vo_evil/holy_shit")
                         self.msg(msg)
                     elif distance > record[0][1]:
-                        msg = "^1New map record^7! {} killed {} from a distance of: ^1{} ^7units.".format(killer_name, victim_name, round(distance))
+                        msg = "^1New map record^7! {} killed {} from a distance of: ^1{} ^7units. Score: ^2{}^7:^2{}".format(killer_name, victim_name, round(distance), killer_score, victim_score)
                         self.play_sound("sound/vo_evil/new_high_score")
                         self.msg(msg)
                         record[0][1] = distance
@@ -71,7 +80,7 @@ class midair(minqlx.Plugin):
             i += 1
 
     def cmd_mytopshots(self, player, msg, channel):
-        x = 5 #how many topshots to list
+        x = 10 #how many topshots to list
         map_name = self.game.map.lower()
         topshots = self.db.zrevrange(PLAYER_KEY.format(player.steam_id) + ":midair:" + str(map_name), 0, x-1, withscores=True)
         player.tell("^7Your ^1midair ^7topshots for map ^1" + map_name + "^7:\n")
@@ -88,8 +97,13 @@ class midair(minqlx.Plugin):
     def cmd_cleartopshots(self, player, msg, channel):
         map_name = self.game.map.lower()
         del self.db[MIDAIR_KEY.format(map_name)]
-        record[0][1] = 0.0
+        record[0] = (['bla', 0.0])
         channel.reply("Topshots for map ^1{} ^7were cleared.".format(map_name))
+
+    def cmd_clearmytopshots(self, player, msg, channel):
+        map_name = self.game.map.lower()
+        del self.db[PLAYER_KEY.format(player.steam_id) + ":midair:" + str(map_name)]
+        channel.reply("Your topshots for map ^1{} ^7were cleared.".format(map_name))
 
     def handle_map(self, map_name, factory):
         global record
