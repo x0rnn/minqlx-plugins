2e49e9680a6caf0a33af0f4415b9f8a4ae592318
x0rnn <c0rnn@live.com>
2016-01-22 18:26:57 +0100

    Update checkplayers.py

--- checkplayers.py
+++ checkplayers.py
@@ -1,4 +1,7 @@
-# checkplayers.py, a plugin to list all banned (excluding leaverbanned) and silenced players
+# checkplayers.py, a plugin to list all players with permission > 1, banned (excluding leaverbanned) and silenced players
+# !permissions
+# !banned
+# !silenced
 
 import minqlx
 import time
@@ -12,6 +15,9 @@ class checkplayers(minqlx.Plugin):
     def __init__(self):
         self.add_command("silenced", self.cmd_silenced, 4)
         self.add_command("banned", self.cmd_banned, 4)
+        self.add_command("permissions", self.cmd_permissions, 4)
+
+        self.adminlist = []
 
     def cmd_silenced(self, player, msg, channel):
         playerlist = self.db.keys("minqlx:players:*:silences")
@@ -68,3 +74,28 @@ class checkplayers(minqlx.Plugin):
                     else:
                         player.tell("^6{} ^7(^6{}^7): ^6{}^7.".format(steamids[i], id_name, expires))
             i += 1
+
+    def cmd_permissions(self, player, msg, channel):
+        playerlist = self.db.keys("minqlx:players:*:permission")
+        tmp = ""
+        tmp2 = ""
+
+        for sublist in playerlist:
+            tmp = str(sublist).split(":")
+            tmp2 += str(tmp[2]) + ","
+        tmp2.split(",")
+        tmp2 = tmp2[:-1]
+        
+        i = 0
+        player.tell("^2Permission levels:\n")
+        for steamids in playerlist:
+            steamids = tmp2.split(",")
+            if self.db.has_permission(steamids[i], 1):
+                id_name = self.db.lindex(PLAYER_KEY.format(steamids[i]), -1)
+                perm = self.db.get(PLAYER_KEY.format(steamids[i]) + ":permission")
+                self.adminlist.append((id_name, steamids[i], perm))
+            i += 1
+
+        self.adminlist.sort(key=lambda p: p[2], reverse=True)
+        for id_name, steamid, perm in self.adminlist:
+            player.tell(id_name + " ^7(" + steamid + "^7) ^2Level: ^6" + perm)
