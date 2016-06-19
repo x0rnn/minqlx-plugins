dfefe3cfd3403aecf57edd368702898d75dd225f
x0rnn <c0rnn@live.com>
2016-06-20 00:57:58 +0200

    Update addbot.py

--- addbot.py
+++ addbot.py
@@ -3,6 +3,7 @@
 # !addbot <thinktime>
 # !botskill <thinktime>
 # server.cfg should have the following cvars set: bot_enable 1, bot_nochat 1, bot_challenge 1
+# note that adding a bot to a game will prevent the game from being ranked by qlstats.net
 # by Mino
 
 import minqlx
