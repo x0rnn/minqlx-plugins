841223093ad95117b1939401b68a026dcc3f0296
x0rnn <c0rnn@live.com>
2016-06-25 12:26:25 +0200

    Added !specall to show who everyone is spectating

--- specs.py
+++ specs.py
@@ -1,13 +1,29 @@
 #specs.py by x0rnn to list players spectating you and to check who someone is spectating
+#!specs: show players spectating you
+#!specwho <id>: show who <id> is spectating
+#!specall: show who every spectator is spectating
+
 import minqlx
+from collections import defaultdict
 
 class specs(minqlx.Plugin):
 
     def __init__(self):
+        self.add_hook("player_disconnect", self.handle_player_disconnect)
         self.add_command("specs", self.cmd_specs)
         self.add_command("specwho", self.cmd_specwho, usage="<id>")
+        self.add_command("specall", self.cmd_specall)
 
+        self.specpos = defaultdict(dict)
+        self.playerpos = defaultdict(dict)
         self.match = False
+
+    def handle_player_disconnect(self, player, reason):
+        try:
+            del self.specpos[str(player.steam_id)]
+            del self.playerpos[str(player.steam_id)]
+        except KeyError:
+            return
 
     def cmd_specs(self, player, msg, channel):
         if player.team == "spectator":
@@ -46,10 +62,24 @@ class specs(minqlx.Plugin):
                     playerz = int(pl.state.position.z)
                     if abs(specx - playerx) < 20 and abs(specy - playery) < 20 and abs(specz - playerz) < 20:
                         self.match = True
+                        name = pl.name
             if self.match:
-                player.tell("{} is spectating {}".format(target_player.name, pl.name))
+                player.tell("{} is spectating {}".format(target_player.name, name))
                 self.match = False
             else:
                 player.tell("{} is not spectating anyone.".format(target_player.name))
         else:
             player.tell("{} is not a spectator.".format(target_player.name))
+
+    def cmd_specall(self, player, msg, channel):
+        for p in self.teams()["spectator"]:
+            self.specpos[str(p.steam_id)]["x"] = int(p.state.position.x)
+            self.specpos[str(p.steam_id)]["y"] = int(p.state.position.y)
+            self.specpos[str(p.steam_id)]["z"] = int(p.state.position.z)
+            for pl in self.players():
+                if pl.team != "spectator":
+                    self.playerpos[str(pl.steam_id)]["x"] = int(pl.state.position.x)
+                    self.playerpos[str(pl.steam_id)]["y"] = int(pl.state.position.y)
+                    self.playerpos[str(pl.steam_id)]["z"] = int(pl.state.position.z)
+                    if abs(self.specpos[str(p.steam_id)]["x"] - self.playerpos[str(pl.steam_id)]["x"]) < 20 and abs(self.specpos[str(p.steam_id)]["y"] - self.playerpos[str(pl.steam_id)]["y"]) < 20 and abs(self.specpos[str(p.steam_id)]["z"] - self.playerpos[str(pl.steam_id)]["z"]) < 20:
+                        player.tell("{} is spectating {}".format(p.name, pl.name))
