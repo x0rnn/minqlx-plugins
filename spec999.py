63d28fee32862e3a47352aeca974231c6cfbc151
x0rnn <c0rnn@live.com>
2016-12-10 11:44:03 +0100

    Update spec999.py

--- spec999.py
+++ spec999.py
@@ -8,7 +8,7 @@ class spec999(minqlx.Plugin):
 
     def cmd_spec999(self, player, msg, channel):
         for p in self.players():
-            if p.ping >= 999:
+            if p.ping >= 990:
                 if p.team != "spectator":
                     p.put("spectator")
                     self.msg("spec999: Moving {} to spectators.".format(p.clean_name))
