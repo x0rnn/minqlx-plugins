161fbb5b3e68f34e3939795361de145e0fd8d037
x0rnn <c0rnn@live.com>
2022-12-10 17:47:43 +0100

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,51 +1,58 @@
 # tts.py by x0rnn, primitive TTS (text-to-speech) system based on arpabet (https://en.wikipedia.org/wiki/Arpabet)
+# Put https://github.com/x0rnn/minqlx-plugins/blob/master/tts.txt in the same directory as this plugin
 # Use with: https://steamcommunity.com/sharedfiles/filedetails/?id=710007616
 # Add 710007616 to qlx_workshopReferences and workshop.txt
 
 import minqlx
 import threading
-import requests
-import json
 import time
 import re
+import csv
 
 class tts(minqlx.Plugin):
     def __init__(self):
-        self.regex = re.compile('[^a-zA-Z]')
         self.add_command("tts", self.cmd_tts, 1, usage="<text>")
+        self.words_list = []
+        self.phonemes_list = []
+
+        with open("tts.txt", "r", encoding="utf8") as ph:
+            tsv_reader = csv.DictReader(ph, delimiter="\t")
+            for w in tsv_reader:
+                words_list.append(w["Word"])
+                phonemes_list.append(w["Phonemes"])
 
     def cmd_tts(self, player, msg, channel):
         if len(msg) < 1:
             return minqlx.RET_USAGE
 
         text = " ".join(msg[1:])
-        self.getPhonemes(text)
+        self.getPhonemes(text, player)
         return minqlx.RET_STOP_ALL
 
     @minqlx.thread
-    def getPhonemes(self, text):
-        def tts(list):
+    def getPhonemes(self, text, player):
+        valid_words = []
+        invalid_words = []
+        phonemes = []
+
+        def tts(ph_list):
             for phoneme in list:
-                if phoneme not in ",.?!":
-                    phoneme = self.regex.sub('', phoneme)
-                    self.play_sound("sound/tts/" + phoneme + ".wav")
-                    time.sleep(0.15)
+                self.play_sound("sound/tts/" + phoneme + ".wav")
+                time.sleep(0.15)
 
-        try:
-            url = "http://api.corrasable.com/phonemes"
-            params = { "text": text }
-            res = requests.post(url, params=params)
-        except requests.exceptions.RequestException:
-            return
+        text = re.sub(r'[.,"\-?:!;0-9]', '', text)
+        text_list = text.split()
+        for w in text_list:
+            if w in words_list:
+                valid_words.append(w)
+            else:
+                invalid_words.append(w)
 
-        res = res.json()
-        res = res[0]
-        try:
-            res.remove('N/A')
-        except:
-            pass
-        string = " ".join([str(x) for x in res])
-        string = "".join(i for i in string if not i.isdigit())
-        list = string.split()
-
-        tts(list)
+        if len(invalid_words) != 0:
+            player.tell("Didn't find the following words: '{}', can't do TTS.".format(' '.join(invalid_words)))
+        else:
+            for w in valid_words:
+                phonemes.append(phonemes_list[words_list.index(w)])
+            ph_string = ' '.join(phonemes)
+            ph_list = ph_string.split()
+            tts(ph_list)
